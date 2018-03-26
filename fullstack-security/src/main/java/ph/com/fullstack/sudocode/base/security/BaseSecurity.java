eadcfd254626a94a973ccf520d37eb9503f9ab57
TEUSER <Jomer.Pangilinan@tractebel.engie.com>
2018-03-26 19:16:25 +0800

    [DEV] - Addded logging dependency

--- fullstack-security/src/main/java/ph/com/fullstack/sudocode/base/security/BaseSecurity.java
+++ fullstack-security/src/main/java/ph/com/fullstack/sudocode/base/security/BaseSecurity.java
@@ -1,11 +1,15 @@
 package ph.com.fullstack.sudocode.base.security;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class BaseSecurity extends WebSecurityConfigurerAdapter {
+
+    private static final Logger LOGGER = LogManager.getLogger(BaseSecurity.class);
 
     public BaseSecurity() {
     }
@@ -16,6 +20,7 @@ public class BaseSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        LOGGER.info("configure(HttpSecurity http) : START");
         super.configure(http);
     }
 }
