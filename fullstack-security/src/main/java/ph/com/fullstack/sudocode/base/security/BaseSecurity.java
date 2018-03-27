b3ec21d38a8165cfa334ea4f3c4b4372c8ba75db
TEUSER <Jomer.Pangilinan@tractebel.engie.com>
2018-03-27 14:35:31 +0800

    [DEV] - Add basic security for actuator

--- fullstack-security/src/main/java/ph/com/fullstack/sudocode/base/security/BaseSecurity.java
+++ fullstack-security/src/main/java/ph/com/fullstack/sudocode/base/security/BaseSecurity.java
@@ -2,6 +2,8 @@ package ph.com.fullstack.sudocode.base.security;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +23,12 @@ public class BaseSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         LOGGER.info("configure(HttpSecurity http) : START");
-        super.configure(http);
+
+        http.authorizeRequests()
+                .requestMatchers(EndpointRequest.to("health")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/user").permitAll().and().httpBasic();
+
     }
 }
